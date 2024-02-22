28ee9212f3765b97f70a041a4c3f2fb18fbbdb6a
Michael Wang <41971562+devmwang@users.noreply.github.com>
2024-02-22 02:51:31 -0800

    Refactor

--- gtap/coordinator.py
+++
@@ -1,5 +0,0 @@
-from gtap.process.process import PythonProcess
-
-processes = [PythonProcess("DisplayCapture", "gtap.display_capture.display_capture")]
-
-system_processes = {process.name: process for process in processes}
---
+++ gtap/coordinator/coordinator.py
@@ -0,0 +1,10 @@
+from gtap.coordinator.process import startAllProcesses, waitForProcesses, PythonProcess
+
+processes = [PythonProcess("DisplayCapture", "gtap.display_capture.display_capture")]
+
+system_processes = {process.name: process for process in processes}
+
+
+def main():
+    startAllProcesses(processes)
+    waitForProcesses(processes)

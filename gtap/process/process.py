46231b2d359edeb746b9b20382320c3ebd95796c
Michael Wang <41971562+devmwang@users.noreply.github.com>
2024-02-22 02:39:16 -0800

    Setup async/process system

---
+++ gtap/process/process.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+from multiprocessing import Process
+from importlib import import_module
+from setproctitle import setproctitle
+
+def launcher(module, name):
+    module = import_module(module)
+    
+    setproctitle(name)
+    
+    module.main()
+
+class BaseProcess(ABC):
+    name = ""
+    process = None
+
+    @abstractmethod
+    def start(self):
+        pass
+
+    def restart(self):
+        self.stop()
+        self.start()
+
+    def stop(self):
+        if self.process == None:
+            return None
+        
+        # TODO: Initiate process shutdown and confirm
+
+class PythonProcess(BaseProcess):
+    def __init__(self, name, module):
+        self.name = name
+        self.module = module
+
+    def start(self):
+        if self.process != None:
+            return None
+        
+        self.process = Process(name=self.name, target=launcher, args=(self.module, self.name))
+        self.process.start()
